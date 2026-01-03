f1e2815c87649d4e7380911f3a7ab9f32411da64
seran261 <seran261@gmail.com>
2026-01-03 14:11:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
 import asyncio
-from binance_data import fetch_ohlcv
+from exchange_data import fetch_ohlcv
 from strategy import check_signal
 from telegram_bot import send_signal
-
-TOP_COINS = [...]
+from config import SYMBOLS, TIMEFRAME, SCAN_DELAY
 
 async def run():
+    print("🚀 Bot started")
+
     while True:
-        for symbol in TOP_COINS:
-            df = fetch_ohlcv(symbol)
+        for symbol in SYMBOLS:
+            df = fetch_ohlcv(symbol, TIMEFRAME)
+
+            if df is None or df.empty:
+                continue
+
             signal = check_signal(df)
             if signal:
                 side, entry, tp, sl = signal
                 await send_signal(symbol, side, entry, tp, sl)
                 await asyncio.sleep(10)
-        await asyncio.sleep(60)
+
+        await asyncio.sleep(SCAN_DELAY)
 
 asyncio.run(run())
