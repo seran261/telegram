13c290a650a6e523726b7184495a0b4224924e93
seran261 <seran261@gmail.com>
2026-01-03 14:04:57 +0530

    Update binance_data.py

--- binance_data.py
+++ binance_data.py
@@ -1,14 +1,21 @@
 import ccxt
 import pandas as pd
 
-exchange = ccxt.binance({
+exchange = ccxt.binanceusdm({
     "enableRateLimit": True,
-    "options": {"defaultType": "future"}
+    "options": {
+        "defaultType": "future"
+    }
 })
 
-def fetch_ohlcv(symbol, timeframe="5m", limit=150):
-    data = exchange.fetch_ohlcv(symbol, timeframe, limit=limit)
-    df = pd.DataFrame(
-        data, columns=["time","open","high","low","close","volume"]
-    )
-    return df
+def fetch_ohlcv(symbol, timeframe="5m", limit=200):
+    try:
+        data = exchange.fetch_ohlcv(symbol, timeframe, limit=limit)
+        df = pd.DataFrame(
+            data,
+            columns=["time", "open", "high", "low", "close", "volume"]
+        )
+        return df
+    except Exception as e:
+        print(f"Fetch error {symbol}: {e}")
+        return None
